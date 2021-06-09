5126e4287a6cd24d6a8c0e16360cd1cac3d43cca
Dussasathvik <82590968+Dussasathvik@users.noreply.github.com>
2021-06-09 15:49:16 +0530

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,4 +1,3 @@
-//import org.json.simple.JSONObject;
 import java.util.Scanner;
 
 class Quiz {
@@ -7,85 +6,94 @@ class Quiz {
     private int score[] = new int[5];
     private int i = 0;
 
-    /*
-     * org.json.JSONObject jsonObject = (org.json.JSONObject) obj;
-     * 
-     * String name = (String) jsonObject.get("name"); double salary = (Double)
-     * jsonObject.get("salary"); long age = (Long) jsonObject.get("age");
-     * System.out.println(name+" "+salary+" "+age);
-     */
-    void Enterntainment() {
-        int result[] = { 1, 2, 1, 3, 4, 5 };
+    void politics() {
+        int result[] = { 1, 2, 3, 4, 4 };
         int count = 0;
-        String question = "question";
-        String questionoption = "1.one\t2.tw\n3.three\t4.four";
         for (; i < 5; i++) {
+            if (i == 0) {
+                System.out.println(i + ".\twho is the chief minister,All the portfolios not allocated to any Minister of telangana");
+                System.out.println("1.Sri Kalvakuntla Chandrashekar Rao \t2.Sri Md. Mohamood Ali \n3.Sri A. Indrakaran Reddy \t4.Sri Talasani Srinivas Yadav");
+            } else if (i == 1) {
+                System.out.println(i + ".\twho is the home minister,Prisons and Fire Services minister of telangana");
+                System.out.println("1.Sri Kalvakuntla Chandrashekar Rao \t2.Sri Md. Mohamood Ali \n3.Sri A. Indrakaran Reddy \t4.Sri Talasani Srinivas Yadav");
+           } else if (i == 2) {
+                System.out.println(i + ".\twho is the Forest & Environment and S&T, Endowments and Law minister of telanganaa");
+                System.out.println("1.Sri Kalvakuntla Chandrashekar Rao \t2.Sri Md. Mohamood Ali \n3.Sri A. Indrakaran Reddy \t4.Sri Talasani Srinivas Yadav");
+            } else if (i == 3) {
+                System.out.println(i + ".\twho is the Animal Husbandry, Fisheries, Dairy Development Corp. and Cinematography minister of telangana");
+                System.out.println("1.Sri Kalvakuntla Chandrashekar Rao \t2.Sri Md. Mohamood Ali \n3.Sri A. Indrakaran Reddy \t4.Sri Talasani Srinivas Yadav");
+            } else if (i == 4) {
+                System.out.println(i + ".\twho is the Finance minister of telangana");
+                System.out.println("1.kejriwal \t2.chandrashekar roa\n3.Jagan mohan \t 4.Sri Thanneeru Harish Rao");
+            }
 
-            System.out.println("Question " + i + "\n " + question + ":\n1." + questionoption + "\nEnter an option");
             score[i] = sc.nextInt();
-            System.out.printf("", score[i] == result[i] ? "true" + count + 1 : "false" + count--);
-
+            if (score[i] == result[i]) {
+                System.out.println("\n" + i + ".True");
+                count++;
+            } else {
+                System.out.println("\n" + i + ".False");
+                count--;
+            }
+            // score[i].equalsto(result[i]) ? "true" + count + + : "false" + count - -;
         }
         System.out.println("Result is " + count);
     }
 
-    private void Sports() {
-        int result[] = { 1, 2, 1, 3, 4, 5 };
+    private void movies() {
+        int result[] = { 1, 2, 1, 3, 4 };
         int count = 0;
-        String question = "question";
-        String questionoption = "1.one\t2.tw\n3.three\t4.four";
         for (; i < 5; i++) {
+            if (i == 0) {
+                System.out.println(i + ".\t what is the first movie of hero naga chaitanya");
+                System.out.println("1.josh \t2. bezawada\n3.durga \t4.dhada");
+            } else if (i == 1) {
+                System.out.println(i + ".\t what is the first movie of hero mahesh babu");
+                System.out.println("1.yuvaraja\t2.rajakumarudu\n3.murari \t 4.bobby");
+            } else if (i == 2) {
+                System.out.println(i + ".\twhat is the first movie of hero nithin");
+                System.out.println("1.jayam \t2.dil \n3.hero\t 4.sambaram");
+            } else if (i == 3) {
+                System.out.println(i + ".\twhat is the first movie of hero ram charan");
+                System.out.println("1.orange \t2.racha \n3.chirutha \t 4.magadheera");
+            } else if (i == 4) {
+                System.out.println(i + ".\twhat is the first movie of hero varun teja");
+                System.out.println("1.loafer \t2.kanche \n3.hands up \t 4.mukunda");
+            }
 
-            System.out.println("Question " + i + "\n " + question + ":\n1." + questionoption + "Enter an option");
             score[i] = sc.nextInt();
-            System.out.printf("", score[i] == result[i] ? "true" + count + 1 : "false" + count--);
-
+            if (score[i] == result[i]) {
+                System.out.println("\n" + i + ".True");
+                count++;
+            } else {
+                System.out.println("\n" + i + ".False");
+                count--;
+            }
+            // score[i].equalsto(result[i]) ? "true" + count + + : "false" + count - -;
         }
         System.out.println("Result is " + count);
+
     }
 
     private void pyschology() {
-        int result[] = { 1, 2, 1, 3, 4, 5 };
-        int count = 0;
-        String question = "question";
-        String questionoption = "1.one\t2.tw\n3.three\t4.four";
-        for (; i < 5; i++) {
 
-            System.out.println("Question " + i + "\n " + question + ":\n1." + questionoption + "Enter an option");
-            score[i] = sc.nextInt();
-            System.out.printf("", score[i] == result[i] ? "true" + count + 1 : "false" + count--);
-
-        }
-        System.out.println("Result is " + count);
     }
 
     private void Gs() {
-        int result[] = { 1, 2, 1, 3, 4, 5 };
-        int count = 0;
-        String question = "question";
-        String questionoption = "1.one\t2.tw\n3.three\t4.four";
-        for (; i < 5; i++) {
 
-            System.out.println("Question " + i + "\n " + question + ":\n1." + questionoption + "Enter an option");
-            score[i] = sc.nextInt();
-            System.out.printf("", score[i] == result[i] ? "true" + count + 1 : "false" + count--);
-
-        }
-        System.out.println("Result is " + count);
     }
 
     Quiz() {
-        System.out.println("QUiz application");
-        System.out.println(
-                "*******>>\n1.Entertainment\t\t2.Sports\n3.General science\t4.Pyschology\n>>>>**<<<<");
+        System.out.println(">>>  WELCOME TO QUIZ <<<");
+        System.out.println("**\n1.politics\t\t2.movies\n3.General science\t4.Pyschology\n>>>>  SELECT ANY ONE <<<<");
         int input = sc.nextInt();
         Thread th = new Thread() {
             public void run() {
                 try {
                     if (input == 1) {
-                        Enterntainment();
+                        politics();
                     } else if (input == 2) {
-                        Sports();
+                        movies();
                     } else if (input == 3) {
                         Gs();
                     } else {
